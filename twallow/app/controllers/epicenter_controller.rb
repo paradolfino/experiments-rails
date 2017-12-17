dc4a87b62b158f4783c9fc27c1616c258a3a8b98
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-17 14:08:17 -0600

    modify epicenter controller with now_following

--- twallow/app/controllers/epicenter_controller.rb
+++ twallow/app/controllers/epicenter_controller.rb
@@ -15,6 +15,13 @@ class EpicenterController < ApplicationController
   end
 
   def now_following
+    # We are adding the user.id of the user you want to 
+    # follow to your following array.
+    # and we want to make sure it's saved in there as an integer
+    current_user.following.push(params[:id].to_i)
+    current_user.save
+  
+    redirect_to show_user_path(id: params[:id])
   end
 
   def unfollow
