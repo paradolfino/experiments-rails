722e56c14f9a5af378b4fe00bd38b4c7f2ff536c
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-23 20:05:20 -0600

    beta-blog: edit: tmp

--- beta-blog/config/routes.rb
+++ beta-blog/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'pages#home'
+  root '/', to: 'pages#home'
   get '/about', to: 'pages#about'
 end
