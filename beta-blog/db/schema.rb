a6e02ed85cba456428f185a5a9d05372f50ff868
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-25 19:29:25 -0600

    commit to pool[x6gbe6] at 19:29

--- beta-blog/db/schema.rb
+++ beta-blog/db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171226012240) do
+ActiveRecord::Schema.define(version: 20171226012647) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
