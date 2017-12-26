2062444dc647965cb71f4731bed1950c803f3d30
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-25 19:18:44 -0600

    commit to pool[x6gbe6] at 19:18

--- gitreaper.rb
+++ gitreaper.rb
@@ -1,5 +1,9 @@
 class GitReaper
 
+    @@color_red = "\033[31m"
+    @@color_green = "\033[32m"
+    @@color_default = "\033[0m"
+
     def initialize
         
     end
@@ -13,9 +17,9 @@ class GitReaper
     def self.execute(param)
         stalker = %x{#{param}}
         if stalker.include? "nothing to commit" 
-            puts "Stalking"
+            puts @@color_red + "stalking" + @@color_default
         elsif stalker.include? "insert"
-            puts stalker
+            puts @@color_green + stalker + @@color_default
         end
 
     end
