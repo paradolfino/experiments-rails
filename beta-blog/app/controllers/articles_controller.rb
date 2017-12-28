2af1fc7117114c6e88d765c1bcec7b326cb8b3d7
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-27 19:43:33 -0600

    commit 18 to pool[rapid-living-river-cny7h8] at 19:43 - 27/12/2017

--- beta-blog/app/controllers/articles_controller.rb
+++ beta-blog/app/controllers/articles_controller.rb
@@ -7,6 +7,7 @@ class ArticlesController < ApplicationController
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
         @article.save
+        redirect_to articles_show(@article)
     end
 
     private
