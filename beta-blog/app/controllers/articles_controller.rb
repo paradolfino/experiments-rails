4b2268b36ad53c920de462b2b17708a5e56adf4c
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-31 17:05:20 -0600

    commit 3 to pool[pretty-shining-river-h33t51] at 17:05 - 31/12/2017

--- beta-blog/app/controllers/articles_controller.rb
+++ beta-blog/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article
+
     def index
         @articles = Article.all
     end
@@ -9,12 +11,12 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        set_article
+        
     end
 
     def create
         #render plain: params[:article].inspect
-        set_article
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was successfully created"
             redirect_to article_path(@article)
@@ -24,7 +26,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        set_article
+        
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
@@ -34,11 +36,11 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        set_article
+        
     end
 
     def destroy
-        set_article
+        
         @article.destroy
         flash[:notice] = "Article was successfully deleted."
         redirect_to articles_path
