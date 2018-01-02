a0389630f3d1fdd92f0c86a4fdf283a6e15c6ef0
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-01 21:34:47 -0600

    commit 2 to pool[serene-rolling-mountain-z86i7d] at 21:34 - 01/01/2018

--- normangiantribe/db/migrate/20180102033028_create_dictionaries.rb
+++ normangiantribe/db/migrate/20180102033028_create_dictionaries.rb
@@ -1,7 +1,15 @@
 class CreateDictionaries < ActiveRecord::Migration[5.1]
   def change
     create_table :dictionaries do |t|
-
+      t.string :engword
+      t.string :norword
+      t.string :norword_past
+      t.string :norword_pres
+      t.string :norword_sing
+      t.string :norword_plur
+      t.string :norword_sing_def
+      t.string :norword_plur_def
+      t.text :norword_usage
       t.timestamps
     end
   end
