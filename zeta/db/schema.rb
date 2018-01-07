a7c7015292751bd32e572326459f05dd65c165d7
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-07 13:37:55 -0600

    commit 12 to pool[brilliant-steaming-cow-3s7jt3] at 13:37 - 07/01/2018

--- zeta/db/schema.rb
+++ zeta/db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107193437) do
+ActiveRecord::Schema.define(version: 20180107193730) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
