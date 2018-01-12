ed4851ce901b661df1bf38db54fcade8517319e3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-11 20:43:38 -0600

    commit 13 to pool[rapid-living-mountain-p45555] at 20:43 - 11/01/2018

--- normangiantribe/app/models/post.rb
+++ normangiantribe/app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
-    validates :title, presence: true, length { minimum: 3, maximum: 100 }
-    validates :body, presence: true, length { minimum: 100, maximum: 3000 }
+    validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+    validates :body, presence: true, length: { minimum: 100, maximum: 3000 }
+    
 end
