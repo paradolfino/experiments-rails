5a85e83a06a69249394ed51f83772b292c0b0021
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-11 19:54:55 -0600

    commit 5 to pool[pretty-living-cow-1h147a] at 19:54 - 11/01/2018

--- beta-blog/app/models/article.rb
+++ beta-blog/app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-
+  belongs_to :users
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { length: 10, maximum: 300 }
 
