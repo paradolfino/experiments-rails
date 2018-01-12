08cc1d924b7472ef8535c00fe494ea68c3f275a9
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-11 19:41:27 -0600

    commit 6 to pool[rapid-rolling-cow-7oe645] at 19:41 - 11/01/2018

--- beta-blog/app/models/user.rb
+++ beta-blog/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     validates :username, presence: true, 
     uniqueness: { case_sensitive: false }, 
     length: { minimum: 3, maximum: 25 }
+    VALID_EMAIL_REGEX: 
     validates :email, presence: true, 
     length: { maximum: 105 }, 
     uniqueness: { case_sensitive: false }, 
