3eba3b18394f13aa77c4dc8966ddf8e0476a4baf
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-12 20:37:50 -0600

    commit 2 to pool[brilliant-shining-rabbit-0ci651] at 20:37 - 12/01/2018

--- normangiantribe/app/controllers/posts_controller.rb
+++ normangiantribe/app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
     def create
         #render plain: params[:article].inspect
         @post = Post.new
-        #@article.user = User.first
+        #@post.user = User.first -- after add User and associate to Post
         if @article.save
             flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
