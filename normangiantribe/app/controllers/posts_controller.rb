87300a9fcb5a0731a035db8207146e34288d58d6
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-12 20:37:26 -0600

    commit 1 to pool[brilliant-shining-rabbit-0ci651] at 20:37 - 12/01/2018

--- normangiantribe/app/controllers/posts_controller.rb
+++ normangiantribe/app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
+    before_action :set_article, only: [:edit,:update,:show,:destroy]
     def index
         @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def edit
+        
+    end
+
+    def create
+        #render plain: params[:article].inspect
+        @post = Post.new
+        #@article.user = User.first
+        if @article.save
+            flash[:success] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
 end
