90d6d1d9dc764090e63317ab8e11828d9d6eda0b
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-14 18:36:23 -0600

    commit 3 to pool[serene-steaming-mountain-4f7le7] at 18:36 - 14/01/2018

--- beta-blog/config/routes.rb
+++ beta-blog/config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get 'signup', to: "users#new"
+  
 
   resources :articles
   resources :users
+
+  get 'signup', to: "users#new"
 end
