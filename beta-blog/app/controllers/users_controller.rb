570719f957e438606832279d57db4090aab889c3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-14 18:49:34 -0600

    commit 22 to pool[serene-steaming-mountain-4f7le7] at 18:49 - 14/01/2018

--- beta-blog/app/controllers/users_controller.rb
+++ beta-blog/app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-
+            flash[:success] = "Welcome to Elfa Blog, #{@user.username}. "
+            redirect_to articles_path
         else
             render 'new'
         end
