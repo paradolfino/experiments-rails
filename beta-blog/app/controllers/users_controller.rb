6a1df59f484d22bfda538359ff4e00f19839391f
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-15 19:03:58 -0600

    commit 2 to pool[rapid-shining-cow-5v2f35] at 19:03 - 15/01/2018

--- beta-blog/app/controllers/users_controller.rb
+++ beta-blog/app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def update
+
+    end
+
     private
         def user_params
             params.require(:user).permit(:username, :email, :password)
