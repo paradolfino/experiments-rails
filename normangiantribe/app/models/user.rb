b8bf308a405d2b8c90216406e4efac5c00a07ec8
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-16 21:26:45 -0600

    commit 22 to pool[rapid-living-river-5o1ln3] at 21:26 - 16/01/2018

--- normangiantribe/app/models/user.rb
+++ normangiantribe/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :articles
+    has_many :threads
     before_save { self.email = email.downcase }
     validates :username, presence: true, 
     uniqueness: { case_sensitive: false }, 
