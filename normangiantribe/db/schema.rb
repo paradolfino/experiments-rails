a9f8647467676dd4bcac4a33ed15f5181fc88544
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-16 21:23:34 -0600

    commit 12 to pool[rapid-living-river-5o1ln3] at 21:23 - 16/01/2018

--- normangiantribe/db/schema.rb
+++ normangiantribe/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117031932) do
+ActiveRecord::Schema.define(version: 20180112023336) do
 
   create_table "dictionaries", force: :cascade do |t|
     t.string "engword"
@@ -29,13 +29,6 @@ ActiveRecord::Schema.define(version: 20180117031932) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users_tables", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
