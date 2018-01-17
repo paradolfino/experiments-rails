a5d8c08eae1264e9570a70eefcbfbffff303ca67
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-16 21:50:25 -0600

    commit 17 to pool[serene-rolling-cow-k2ns6d] at 21:50 - 16/01/2018

--- normangiantribe/app/controllers/threads_controller.rb
+++ normangiantribe/app/controllers/threads_controller.rb
@@ -1,2 +1,37 @@
 class ThreadsController < ApplicationController
+    before_action :set_thread, only: [:edit,:update,:show,:destroy]
+    def index
+        @threads = thread.all
+    end
+
+    def new
+        @thread = thread.new
+    end
+
+    def edit
+        
+    end
+
+    def create
+        #render plain: params[:thread].inspect
+        @thread = thread.new(thread_params)
+        if @thread.save
+            flash[:success] = "thread was successfully created"
+            redirect_to thread_path(@thread)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @thread = thread.find(params[:id])
+    end
+
+    private
+        def thread_params
+            params.require(:thread).permit(:title, :body)
+        end
+        def set_thread
+            @thread = thread.find(params[:id])
+        end
 end
