b9f373e9221f09e075e9eb9e21e08d7ef2402ba4
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-16 21:20:56 -0600

    commit 6 to pool[rapid-living-river-5o1ln3] at 21:20 - 16/01/2018

--- normangiantribe/db/migrate/20180117031932_create_users_table.rb
+++ normangiantribe/db/migrate/20180117031932_create_users_table.rb
@@ -1,6 +1,9 @@
 class CreateUsersTable < ActiveRecord::Migration[5.1]
   def change
     create_table :users_tables do |t|
+      t.string :username
+      t.string :email
+      t.timestamps
     end
   end
 end
