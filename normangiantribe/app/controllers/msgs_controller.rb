9907c49d4c91ed9d97b0e98a6b42646a14f503a0
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-17 19:34:08 -0600

    commit 11 to pool[pretty-rolling-rabbit-17vr8a] at 19:34 - 17/01/2018

--- normangiantribe/app/controllers/msgs_controller.rb
+++ normangiantribe/app/controllers/msgs_controller.rb
@@ -1,5 +1,14 @@
 class MsgsController < ApplicationController
+    before_action :set_post, only: [:edit,:update,:show,:destroy]
     def index
         @msgs = Msg.all
     end
+
+    private
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
+        def set_post
+            @post = Post.find(params[:id])
+        end
 end
