2044aad37e8ac730020f854c796fefbb39d74fc7
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-17 20:15:44 -0600

    commit 11 to pool[pretty-rolling-river-p661ee] at 20:15 - 17/01/2018

--- commentary/db/migrate/20180118020958_create_posts.rb
+++ commentary/db/migrate/20180118020958_create_posts.rb
@@ -3,6 +3,8 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.string :title
       t.text :body
+      t.user_id :integer
+      t.categor_id :integer
       t.timestamps
     end
   end
