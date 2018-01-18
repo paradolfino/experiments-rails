a1b9e2046eb4f0d6d0b4bf406f3e4f9880d7a6c6
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-17 20:23:56 -0600

    commit 20 to pool[pretty-rolling-river-p661ee] at 20:23 - 17/01/2018

--- commentary/app/models/post.rb
+++ commentary/app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :body, presence: true, length: { length: 10, maximum: 300 }
     validates :user_id, presence: true
+    validates :category_id, presence: true
 end
