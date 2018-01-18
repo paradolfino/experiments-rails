4188039eab6a2e89a2c71bef72ed7b937b8586c7
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-17 19:26:22 -0600

    commit 10 to pool[pretty-rolling-rabbit-17vr8a] at 19:26 - 17/01/2018

--- normangiantribe/app/controllers/users_controller.rb
+++ normangiantribe/app/controllers/users_controller.rb
@@ -29,9 +29,9 @@ class UsersController < ApplicationController
         end
     
         def show
-            @msgs = Msg.all
+            
             @user = User.find(params[:id])
-            @articles = @user.articles
+            @msgs = @user.msgs
         end
     
         def index
