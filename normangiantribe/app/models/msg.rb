272f383cf9f24dae11eba7a7c1c9a17be0b8e154
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-17 18:44:17 -0600

    commit 7 to pool[serene-rolling-rabbit-85g8rv] at 18:44 - 17/01/2018

--- normangiantribe/app/models/msg.rb
+++ normangiantribe/app/models/msg.rb
@@ -1,2 +1,6 @@
 class Msg < ApplicationRecord
+    belongs_to :user
+    validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+    validates :body, presence: true, length: { minimum: 5, maximum: 3000 }
+    
 end
