8bf445f83d38c8a001b6f0e86a04065160012ff5
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-18 20:45:42 -0600

    commit 29 to pool[brilliant-steaming-mountain-at1vvy] at 20:45 - 18/01/2018

--- beta-blog/app/controllers/sessions_controller.rb
+++ beta-blog/app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id 
             flash.now[:success] = 'Logged in!'
-            redirect_to users_path(user)
+            redirect_to user_path(user)
         else
             flash.now[:danger] = 'There was something wrong with your credentials.'
             render 'new'
