775fbb321e584fe492eb7eb840085f802360dd8f
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-18 20:38:24 -0600

    commit 17 to pool[brilliant-steaming-mountain-at1vvy] at 20:38 - 18/01/2018

--- beta-blog/app/controllers/sessions_controller.rb
+++ beta-blog/app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
     end
 
     def destroy
