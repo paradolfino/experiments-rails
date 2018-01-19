fce073b99174d88d34e3b63b78c09667afdb0229
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-18 20:09:10 -0600

    commit 5 to pool[pretty-shining-mountain-2xzl1p] at 20:09 - 18/01/2018

--- beta-blog/app/controllers/articles_controller.rb
+++ beta-blog/app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit,:update,:show,:destroy]
 
     def index
-        @articles = Article.all
+        @articles = Article..paginate(page: params[:page])
     end
 
     def new
