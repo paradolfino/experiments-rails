cb97708a980b243c7b3936e91cff2f3ba0944985
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-18 20:40:18 -0600

    commit 21 to pool[brilliant-steaming-mountain-at1vvy] at 20:40 - 18/01/2018

--- beta-blog/app/controllers/sessions_controller.rb
+++ beta-blog/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
-
+            flash.now[:success] = 'Logged in!'
         else
             flash.now[:danger] = 'There was something wrong with your credentials.'
             render 'new'
