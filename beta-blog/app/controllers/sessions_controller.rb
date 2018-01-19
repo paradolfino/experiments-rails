9dd02f67c7a37b044e0c1f814fa274b8337b8caf
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-18 20:41:08 -0600

    commit 23 to pool[brilliant-steaming-mountain-at1vvy] at 20:41 - 18/01/2018

--- beta-blog/app/controllers/sessions_controller.rb
+++ beta-blog/app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
+            session[:user_id]
             flash.now[:success] = 'Logged in!'
             redirect_to users_path(user)
         else
