902730eaa7e589e4276d8ecd04ce5db2bd810cd1
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-20 17:10:43 -0600

    commit 11 to pool[pretty-rolling-cow-alh131] at 17:10 - 20/01/2018

--- rails_portfolio/db/migrate/20180120230719_create_skills.rb
+++
@@ -1,9 +0,0 @@
-class CreateSkills < ActiveRecord::Migration[5.1]
-  def change
-    create_table :skills do |t|
-      t.string :name
-      t.integer :percent_used
-      t.timestamps
-    end
-  end
-end
