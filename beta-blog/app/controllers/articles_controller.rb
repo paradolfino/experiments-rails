bb149c2da374def2f12c47a9167d8a9358470170
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-19 22:03:31 -0600

    commit 8 to pool[pretty-living-river-n2ghw6] at 22:03 - 19/01/2018

--- beta-blog/app/controllers/articles_controller.rb
+++ beta-blog/app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
     before_action :set_article, only: [:edit,:update,:show,:destroy]
+    before_action :require_user, except: [:index, :show ]
 
     def index
         @articles = Article.paginate(page: params[:page], per_page: 5)
