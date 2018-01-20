b324a11e5355318cef469ef876e01b284ca20eeb
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-19 22:08:41 -0600

    commit 15 to pool[pretty-living-river-n2ghw6] at 22:08 - 19/01/2018

--- beta-blog/app/controllers/users_controller.rb
+++ beta-blog/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit,:update,:show]
     before_action :require_user, except: [:index, :show ]
+    before_action :require_same_user, only: [:edit,:update]
     def new
         @user = User.new
     end
