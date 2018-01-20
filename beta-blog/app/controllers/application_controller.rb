f7ad1fa88f3782f248ef4ccc79ed4a683c5b1c0c
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-19 21:08:54 -0600

    commit 8 to pool[pretty-shining-rabbit-w15gqp] at 21:08 - 19/01/2018

--- beta-blog/app/controllers/application_controller.rb
+++ beta-blog/app/controllers/application_controller.rb
@@ -12,6 +12,9 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
   end
 end
