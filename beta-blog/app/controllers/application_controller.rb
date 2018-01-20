229246c14241c7393ae7194c590bc94c9682c22c
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-19 21:04:49 -0600

    commit 1 to pool[pretty-shining-rabbit-w15gqp] at 21:04 - 19/01/2018

--- beta-blog/app/controllers/application_controller.rb
+++ beta-blog/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+
+  end
+
+  def logged_in?
+
+  end
+
+  def require_user
+
+  end
 end
