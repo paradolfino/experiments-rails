9c46c935e697910c004172ecdb047a676d438191
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-19 21:16:01 -0600

    commit 17 to pool[pretty-shining-rabbit-w15gqp] at 21:16 - 19/01/2018

--- beta-blog/app/controllers/users_controller.rb
+++ beta-blog/app/controllers/users_controller.rb
@@ -15,11 +15,10 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
+
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "User was successfully updated"
             render 'edit'
@@ -30,7 +29,6 @@ class UsersController < ApplicationController
 
     def show
         
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -42,6 +40,9 @@ class UsersController < ApplicationController
     private
         def user_params
             params.require(:user).permit(:username, :email, :password)
+        end
+        def set_user
+            @user = User.find(params[:id])
         end
 end
   
