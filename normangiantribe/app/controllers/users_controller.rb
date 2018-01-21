7063741447b37d95c3de8c535a59f05036ca328c
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-20 21:01:36 -0600

    commit 31 to pool[rapid-shining-rabbit-lnk03g] at 21:01 - 20/01/2018

--- normangiantribe/app/controllers/users_controller.rb
+++ normangiantribe/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit,:update,:show, :destroy]
-    before_action :require_user, except: [:index, :show, :new, :create ]
+    before_action :require_user, except: [:new, :create ]
     before_action :require_same_user, only: [:edit,:update, :destroy]
     before_action :require_admin, only: [:destroy]
         def new
