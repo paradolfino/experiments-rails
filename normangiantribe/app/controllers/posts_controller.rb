2803ce7fbdab3b348bc80def38fa476046d9c153
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-20 20:56:23 -0600

    commit 28 to pool[rapid-shining-rabbit-lnk03g] at 20:56 - 20/01/2018

--- normangiantribe/app/controllers/posts_controller.rb
+++ normangiantribe/app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
     before_action :set_post, only: [:edit,:update,:show,:destroy]
+    before_action :require_user, except: [:index, :show, :new, :create ]
+    before_action :require_admin, only: [:destroy]
     def index
         @posts = Post.all
     end
