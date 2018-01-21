d3f68924b9f0da8428ca48278ee65f90281f5d8a
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-20 19:53:35 -0600

    commit 22 to pool[rapid-living-rabbit-0aby27] at 19:53 - 20/01/2018

--- beta-blog/app/controllers/users_controller.rb
+++ beta-blog/app/controllers/users_controller.rb
@@ -60,5 +60,11 @@ class UsersController < ApplicationController
                 redirect_to root_path
             end
         end
+        def require_admin
+            if !current_user.admin?
+                flash[:danger] = "You need elevated permissions to do that!"
+                redirect_to root_path
+            end
+        end
 end
   
