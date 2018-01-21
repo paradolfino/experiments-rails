a78fb246036df7142b6c861367bc9d36aca47fd3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-20 20:39:05 -0600

    commit 16 to pool[rapid-shining-rabbit-lnk03g] at 20:39 - 20/01/2018

--- normangiantribe/app/controllers/users_controller.rb
+++ normangiantribe/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit,:update,:show, :destroy]
 
         def new
             @user = User.new
@@ -15,11 +16,10 @@ class UsersController < ApplicationController
         end
     
         def edit
-            @user = User.find(params[:id])
+
         end
     
         def update
-            @user = User.find(params[:id])
             if @user.update(user_params)
                 flash[:success] = "User was successfully updated"
                 redirect_to user_path(@user)
@@ -29,8 +29,6 @@ class UsersController < ApplicationController
         end
     
         def show
-            
-            @user = User.find(params[:id])
             @msgs = @user.msgs
         end
     
@@ -43,7 +41,7 @@ class UsersController < ApplicationController
                 params.require(:user).permit(:username, :email, :password)
             end
             def set_user
-
+                @user = User.find(params[:id])
             end
       
 end
