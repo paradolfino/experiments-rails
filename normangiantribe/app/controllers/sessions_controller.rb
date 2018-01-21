15245861819880e9fc338efb366625f5e455e611
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-20 20:31:47 -0600

    commit 12 to pool[rapid-shining-rabbit-lnk03g] at 20:31 - 20/01/2018

--- normangiantribe/app/controllers/sessions_controller.rb
+++ normangiantribe/app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+    def new
+
+    end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id 
+            flash.now[:success] = 'Logged in!'
+            redirect_to user_path(user)
+        else
+            flash.now[:danger] = 'There was something wrong with your credentials.'
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash.now[:success] = 'Logged out!'
+        redirect_to root_path
+    end
+
 end
+  
