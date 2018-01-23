539e2f656c9a4206a23fcd7a6d107577d04ca1d1
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-22 19:09:11 -0600

    commit 2 to pool[brilliant-steaming-cow-q32u2x] at 19:09 - 22/01/2018

--- rails_portfolio/db/seeds.rb
+++ rails_portfolio/db/seeds.rb
@@ -5,5 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+arr = (a..z).to_a
 rand(25).times { |blog| Blog.create!(title: "My Blog #{blog}!") }
+rand(5).times { |skill| Skill.create!(title: "My Blog #{blog}!") }
