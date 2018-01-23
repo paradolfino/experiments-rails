7958aa53e6e0561c2b13a56f9855bac861de8c41
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-22 19:12:41 -0600

    commit 6 to pool[brilliant-steaming-cow-q32u2x] at 19:12 - 22/01/2018

--- rails_portfolio/db/seeds.rb
+++ rails_portfolio/db/seeds.rb
@@ -8,3 +8,10 @@
 arr = (a..z).to_a
 rand(25).times { |blog| Blog.create!(title: "My Blog #{blog}!", body: "#Test Content") }
 rand(5).times { |skill| Skill.create!(title: "My Skill #{skill}!", percent_utilized: rand(100)) }
+
+rand(9).times do |p|
+    Portfolio.create!(
+        title: "Title #{p}",
+        subtitle: "Subtitle #{p}"
+        )
+end
