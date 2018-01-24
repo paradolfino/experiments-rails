f95bff20f271ea32660f217dfd9895750f95a68d
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-23 20:13:25 -0600

    commit 15 to pool[serene-shining-mountain-4wdt1v] at 20:13 - 23/01/2018

--- beta-blog/app/models/category.rb
+++ beta-blog/app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-    validates :name, presence: true, length: {minimum: 3, maximum: 25}
+    validates :name, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: { case_sensitive: false }
 end
