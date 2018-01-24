44a918958c6c6ecb8ad6bec7e81c7cc27ae813c4
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-23 19:34:51 -0600

    commit 12 to pool[pretty-steaming-rabbit-o3u1iu] at 19:34 - 23/01/2018

--- rails_portfolio/app/controllers/portfolios_controller.rb
+++ rails_portfolio/app/controllers/portfolios_controller.rb
@@ -28,7 +28,7 @@ class PortfoliosController < ApplicationController
     end
 
     def update
-        if @portfolio.update(portfolio_params)
+        if @portfolio.update(params_portfolio)
             flash[:success] = "Portfolio Item was successfully updated!"
             redirect_to portfolio_path(@portfolio)
         else
