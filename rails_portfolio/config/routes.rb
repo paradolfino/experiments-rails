1e99181275f04f5fa1bcd81516753398d878404d
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-24 19:33:33 -0600

    commit 6 to pool[brilliant-steaming-river-n865f1] at 19:33 - 24/01/2018

--- rails_portfolio/config/routes.rb
+++ rails_portfolio/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
