fab1786d54c7e9c032b7f9125c8d65710ccccc2f
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-24 19:48:04 -0600

    commit 8 to pool[brilliant-living-cow-j132d7] at 19:48 - 24/01/2018

--- rails_portfolio/db/schema.rb
+++ rails_portfolio/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125014056) do
+ActiveRecord::Schema.define(version: 20180125014618) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
