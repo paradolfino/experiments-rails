627b8afaa144fa640fe471c053f9ef9c5f73ad34
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-24 20:55:24 -0600

    commit 3 to pool[serene-rolling-mountain-7r3jq5] at 20:55 - 24/01/2018

--- normangiantribe/db/schema.rb
+++ normangiantribe/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118003627) do
+ActiveRecord::Schema.define(version: 20180125025449) do
 
   create_table "dictionaries", force: :cascade do |t|
     t.string "engword"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20180118003627) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
