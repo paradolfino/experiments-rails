f0d419ae273230f88348b60a223ee4738f778b8b
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-24 19:25:00 -0600

    commit 2 to pool[brilliant-steaming-river-n865f1] at 19:25 - 24/01/2018

--- rails_portfolio/config/routes.rb
+++ rails_portfolio/config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  get 'about', to: 'pages/about'
+  get 'about', to: 'pages#about'
 end
