15983cfa65f699213bb3438a5b83ffec089aa4c2
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-24 20:01:26 -0600

    commit 6 to pool[rapid-shining-mountain-5u7280] at 20:01 - 24/01/2018

--- beta-blog/test/controllers/categories_controller_test.rb
+++ beta-blog/test/controllers/categories_controller_test.rb
@@ -3,15 +3,17 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
     test "should get categories index" do
-
+        get :index
+        assert_response :success
     end
 
     test "should get new" do
-
+        get :new
+        assert_response :success
     end
 
     test "should get show" do
-
+        
     end
 
 end
