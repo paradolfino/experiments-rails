3fbcd69a2420f0e76aeb2e39cb8f3a1926d09ee3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-24 20:48:29 -0600

    commit 5 to pool[brilliant-living-cow-cd0ifi] at 20:48 - 24/01/2018

--- beta-blog/test/integration/list_categories_test.rb
+++ beta-blog/test/integration/list_categories_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class ListCategoriesTest < ActionDispatch::IntegrationTest
 
-    
+    def setup
+        @category = Category.create(name: "sports")
+        @category2 = Category.create(name: "programming")
+    end
 
 end
