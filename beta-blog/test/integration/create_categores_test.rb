cd55539affde34a9a0c74d7163cc16e5fc268559
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-24 20:42:18 -0600

    commit 4 to pool[rapid-rolling-river-h73dkb] at 20:42 - 24/01/2018

--- beta-blog/test/integration/create_categores_test.rb
+++ beta-blog/test/integration/create_categores_test.rb
@@ -16,7 +16,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         get new_category_path
         assert_template 'categories/new'
         assert_no_difference 'Category.count' do
-            post categories_path, params: { category: {name: "sports"}}
+            post categories_path, params: { category: {name: ""}}
         end
         assert_template "categories/index"
         assert_match "sports", response.body
