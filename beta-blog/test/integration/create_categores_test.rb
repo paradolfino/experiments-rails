02db5e1360695d75259472939e4a1cc1e5fc2716
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-24 20:14:24 -0600

    commit 4 to pool[serene-steaming-rabbit-23s1nv] at 20:14 - 24/01/2018

--- beta-blog/test/integration/create_categores_test.rb
+++ beta-blog/test/integration/create_categores_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
     test "get new category form and create category" do
-
+        get new_category_path
     end
 
 end
