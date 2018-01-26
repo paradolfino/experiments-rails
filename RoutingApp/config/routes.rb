85a14622007360fdabe53800670c0e2ad93daa40
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 20:37:25 -0600

    commit 27 to pool[pretty-shining-rabbit-7l1tz6] at 20:37 - 25/01/2018

---
+++ RoutingApp/config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  namespace :admin do #nesting
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
+
+  get 'about', to: 'pages#about'
+
+  #get 'contact', to: 'pages#contact'
+
+  get 'leadgen/advertising/landingpage/lead', to: 'pages#contact', as: 'lead'
+
+  root 'pages#home'
+  resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
