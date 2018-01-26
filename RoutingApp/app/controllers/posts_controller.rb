f3aec08f9e7720efd1675c1d161ac80c8f5e88a7
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 20:48:08 -0600

    commit 48 to pool[pretty-shining-rabbit-7l1tz6] at 20:48 - 25/01/2018

--- RoutingApp/app/controllers/posts_controller.rb
+++ RoutingApp/app/controllers/posts_controller.rb
@@ -2,11 +2,16 @@ class PostsController < ApplicationController
     def index
 
     end
+    
     def show
         @post = Post.find(params[:id])
     end
 
     def new
+
+    end
+    
+    def create
 
     end
 
