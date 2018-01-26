4d93a080f306ea92e538f51c34aa656238b53442
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 20:07:57 -0600

    commit 7 to pool[pretty-steaming-river-0ffzl4] at 20:07 - 25/01/2018

--- rails_portfolio/db/schema.rb
+++ rails_portfolio/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126011854) do
+ActiveRecord::Schema.define(version: 20180126020729) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180126011854) do
     t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "skills", force: :cascade do |t|
