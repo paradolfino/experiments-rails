b9aee9be94e8767bca0d7bd5d3c56087fb0f2503
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 20:07:05 -0600

    commit 4 to pool[pretty-steaming-river-0ffzl4] at 20:07 - 25/01/2018

--- rails_portfolio/config/routes.rb
+++ rails_portfolio/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolios, except: [:show]
+  resources :portfolios, except: [:show] do
+    member do
+      get :toggle_status
+    end
+  end
+  
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   resources :blogs do
     member do
