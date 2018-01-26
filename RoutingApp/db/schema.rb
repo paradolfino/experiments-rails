174b3899b2354c27ef327c3c3473b9be80a37d47
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 20:44:12 -0600

    commit 41 to pool[pretty-shining-rabbit-7l1tz6] at 20:44 - 25/01/2018

--- RoutingApp/db/schema.rb
+++ RoutingApp/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126021545) do
+ActiveRecord::Schema.define(version: 20180126024333) do
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
