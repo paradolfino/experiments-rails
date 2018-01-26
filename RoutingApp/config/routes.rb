e6e0c10e5846a8dd18835f2f6de69d702f99f727
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 20:26:11 -0600

    commit 10 to pool[pretty-shining-rabbit-7l1tz6] at 20:26 - 25/01/2018

--- RoutingApp/config/routes.rb
+++ RoutingApp/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   #get 'contact', to: 'pages#contact'
 
   get 'leadgen/advertising/landingpage/lead', to: 'pages#contact', as: 'lead'
+
+  root 'pages#home'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
