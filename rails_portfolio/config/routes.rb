dd1692b483b04d456a21cf3faaf283e66f2cca3d
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 19:50:54 -0600

    commit 16 to pool[pretty-living-rabbit-irae4y] at 19:50 - 25/01/2018

--- rails_portfolio/config/routes.rb
+++ rails_portfolio/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   resources :blogs do
     member do
-      post :toggle_status
+      get :toggle_status
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
