b82e0b1cde08519e366ac0d5932911d92c8434d3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 20:05:55 -0600

    commit 2 to pool[pretty-steaming-river-0ffzl4] at 20:05 - 25/01/2018

--- rails_portfolio/app/models/portfolio.rb
+++ rails_portfolio/app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :body, presence: true, length: { minimum: 3, maximum: 500 }
+    extend FriendlyId
+    friendly_id :title, use: :slugged
 end
