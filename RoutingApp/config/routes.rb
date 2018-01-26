83b7137d4ff486337d3e518b8207a1bbfd64f573
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 20:54:44 -0600

    commit 55 to pool[pretty-shining-rabbit-7l1tz6] at 20:54 - 25/01/2018

--- RoutingApp/config/routes.rb
+++ RoutingApp/config/routes.rb
@@ -21,6 +21,6 @@ Rails.application.routes.draw do
   get 'posts/new', to: 'posts#new', as: 'new_post'
   get 'posts/*missing', to: 'posts#missing'
 
-  get 'query/:'
+  get 'query/:noen'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
