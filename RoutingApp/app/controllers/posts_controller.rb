1a1188f8f9094c745e93d18050ef295050720b6d
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 20:49:26 -0600

    commit 50 to pool[pretty-shining-rabbit-7l1tz6] at 20:49 - 25/01/2018

--- RoutingApp/app/controllers/posts_controller.rb
+++ RoutingApp/app/controllers/posts_controller.rb
@@ -16,6 +16,18 @@ class PostsController < ApplicationController
 
     end
 
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+        
+    end
+
     def missing
 
     end
