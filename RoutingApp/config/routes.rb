d33bd685a30a6b80a767b8c7032199c7e14f6cb4
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-25 20:18:57 -0600

    commit 5 to pool[pretty-shining-rabbit-7l1tz6] at 20:18 - 25/01/2018

--- RoutingApp/config/routes.rb
+++ RoutingApp/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/about'
+  get 'about', to: 'pages#about'
 
-  get 'pages/contact'
+  get 'contact'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
