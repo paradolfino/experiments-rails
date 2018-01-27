330047b937cdc3100f5a1613fad86213d3246b29
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 11:15:40 -0600

    commit 26 to pool[rapid-shining-rabbit-jcyidg] at 11:15 - 27/01/2018

--- normangian_api/app/models/sentence.rb
+++ normangian_api/app/models/sentence.rb
@@ -1,2 +1,8 @@
 class Sentence < ApplicationRecord
+    belongs_to :word
+
+    validates :entry,
+    presence: true,
+    uniqueness: { case_sensitive: false }, 
+    length: { minimum: 3, maximum: 25 }
 end
