5758052901f915eaa5e08cf95614980d5dbd16bd
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 10:23:36 -0600

    commit 2 to pool[rapid-shining-rabbit-jcyidg] at 10:23 - 27/01/2018

--- normangian_api/spec/models/word_spec.rb
+++ normangian_api/spec/models/word_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Word, type: :model do
   # ensure Todo model has a 1:m relationship with the Item model
   it { should have_many(:items).dependent(:destroy) }
   # Validation tests
-  # ensure columns title and created_by are present before saving
+  #ensure columns exist
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:created_by) }
 end
