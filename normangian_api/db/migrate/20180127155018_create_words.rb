49774fb9df048bc4aae1e941f2a23b2615107fec
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 09:57:45 -0600

    commit 15 to pool[serene-rolling-cow-72f1kl] at 09:57 - 27/01/2018

--- normangian_api/db/migrate/20180127155018_create_words.rb
+++ normangian_api/db/migrate/20180127155018_create_words.rb
@@ -4,6 +4,7 @@ class CreateWords < ActiveRecord::Migration[5.1]
       t.string :single
       t.string :plural
       t.string :definite
+      t.string :definite_plural
 
       t.timestamps
     end
