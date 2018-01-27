c5a646455033389c193c123c18b84d5d2bf5fcd0
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 11:19:04 -0600

    commit 29 to pool[rapid-shining-rabbit-jcyidg] at 11:19 - 27/01/2018

--- normangian_api/app/models/word.rb
+++ normangian_api/app/models/word.rb
@@ -2,6 +2,5 @@ class Word < ApplicationRecord
     has_many :sentences, dependent: :destroy
 
     validates :english, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 0, maximum: 25 }
-    validates :single, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 0, maximum: 25 }
     
 end
