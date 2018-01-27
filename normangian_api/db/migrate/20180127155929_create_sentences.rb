f72f98b974f4c542539b5886cd15f6ec0235ee0d
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 10:04:26 -0600

    commit 23 to pool[serene-rolling-cow-72f1kl] at 10:04 - 27/01/2018

--- normangian_api/db/migrate/20180127155929_create_sentences.rb
+++ normangian_api/db/migrate/20180127155929_create_sentences.rb
@@ -2,6 +2,7 @@ class CreateSentences < ActiveRecord::Migration[5.1]
   def change
     create_table :sentences do |t|
       t.text :entry
+      t.references :word, foreign_key: true
       t.timestamps
     end
   end
