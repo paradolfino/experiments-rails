bf405b5c9049dbd94267306fb468ba5821df0526
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 12:04:40 -0600

    commit 19 to pool[rapid-rolling-rabbit-pp47h1] at 12:04 - 27/01/2018

--- normangian_api/app/controllers/words_controller.rb
+++ normangian_api/app/controllers/words_controller.rb
@@ -1,2 +1,44 @@
 class WordsController < ApplicationController
+
+    before_action :set_word, only: [:show, :update, :destroy]
+
+  # GET /words
+  def index
+    @words = word.all
+    json_response(@words)
+  end
+
+  # POST /words
+  def create
+    @word = word.create!(word_params)
+    json_response(@word, :created)
+  end
+
+  # GET /words/:id
+  def show
+    json_response(@word)
+  end
+
+  # PUT /words/:id
+  def update
+    @word.update(word_params)
+    head :no_content
+  end
+
+  # DELETE /words/:id
+  def destroy
+    @word.destroy
+    head :no_content
+  end
+
+  private
+
+  def word_params
+    # whitelist params
+    params.permit(:title, :created_by)
+  end
+
+  def set_word
+    @word = word.find(params[:id])
+  end
 end
