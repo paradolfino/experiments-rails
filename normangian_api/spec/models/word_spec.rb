e1f6ad705b824e967b1160d7e2d8274dbd33213c
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 10:25:57 -0600

    commit 8 to pool[rapid-shining-rabbit-jcyidg] at 10:25 - 27/01/2018

--- normangian_api/spec/models/word_spec.rb
+++ normangian_api/spec/models/word_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Word, type: :model do
   # Association test
-  # ensure Todo model has a 1:m relationship with the Item model
+  # ensure Word model has a 1:m relationship with the Sentence model
   it { should have_many(:sentences).dependent(:destroy) }
   # Validation tests
   #ensure columns exist
