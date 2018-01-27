1e944757cd0c491f86e0156610b95ea9ad80e12e
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 10:26:41 -0600

    commit 9 to pool[rapid-shining-rabbit-jcyidg] at 10:26 - 27/01/2018

--- normangian_api/spec/models/sentence_spec.rb
+++ normangian_api/spec/models/sentence_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Sentence, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure an item record belongs to a single todo record
+  it { should belong_to(:word) }
+  # Validation test
+  # ensure column name is present before saving
+  it { should validate_presence_of(:entry) }
 end
