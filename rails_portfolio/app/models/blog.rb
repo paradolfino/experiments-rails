970335e20caa1444297c9649228933154b80276b
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-29 19:17:00 -0600

    commit 9 to pool[serene-living-cows-s25sx3] at 19:17 - 29/01/2018

--- rails_portfolio/app/models/blog.rb
+++ rails_portfolio/app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
     enum status: { draft: 0, published: 1 }
+    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :body, presence: true, length: { minimum: 3, maximum: 500 }
     extend FriendlyId
     friendly_id :title, use: :slugged
 end
