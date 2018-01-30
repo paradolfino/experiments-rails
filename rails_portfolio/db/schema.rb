1db5c0ad8ee9dd083a9a779f8ec769addbcc6b7e
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-29 19:30:13 -0600

    commit 17 to pool[lovely-baking-cabins-n82owb] at 19:30 - 29/01/2018

--- rails_portfolio/db/schema.rb
+++ rails_portfolio/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126020931) do
+ActiveRecord::Schema.define(version: 20180130012940) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 20180126020931) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
