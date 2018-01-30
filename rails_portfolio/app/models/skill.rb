f91e0162b73b4b57a1c83b0e6ff7f6a748c56202
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-29 19:24:06 -0600

    commit 10 to pool[serene-living-cows-s25sx3] at 19:24 - 29/01/2018

--- rails_portfolio/app/models/skill.rb
+++ rails_portfolio/app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :body, presence: true, length: { minimum: 3, maximum: 500 }
 end
