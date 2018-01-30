541c339f51858d7879dc9caf51aeebe1897db77f
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-29 19:50:01 -0600

    commit 26 to pool[serene-baking-cabins-510mx4] at 19:50 - 29/01/2018

--- rails_portfolio/db/seeds.rb
+++ rails_portfolio/db/seeds.rb
@@ -16,6 +16,7 @@ rand(9).times do |p|
             subtitle: "Subtitle #{p}",
             body: "Body #{p} stuff!!!!",
             main_image: "http://via.placeholder.com/600x400",
-            thumb_image: "http://via.placeholder.com/350x200"
+            thumb_image: "http://via.placeholder.com/350x200",
+            topic_id: Topic.last.id
         )
 end
