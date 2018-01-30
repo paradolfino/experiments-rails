2b6d834ac9815097ef42afbe28d7498844251daa
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-29 19:25:09 -0600

    commit 12 to pool[serene-living-cows-s25sx3] at 19:25 - 29/01/2018

--- rails_portfolio/app/models/portfolio.rb
+++ rails_portfolio/app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :body, presence: true, length: { minimum: 3, maximum: 500 }
+    validates_presence_of :main_image, :thumb_image
     enum status: { draft: 0, published: 1 }
     extend FriendlyId
     friendly_id :title, use: :slugged
