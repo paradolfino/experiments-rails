58c796fc42b9d5f65551c9ea454c9306af358a5a
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-30 19:52:19 -0600

    commit 34 to pool[peaceful-living-horses-a03v0e] at 19:52 - 30/01/2018

--- rails_portfolio/app/models/skill.rb
+++ rails_portfolio/app/models/skill.rb
@@ -6,6 +6,6 @@ class Skill < ApplicationRecord
     after_initialize :set_defaults
 
     def set_defaults
-        self.badge ||= 
+        self.badge ||= Placeholder.image_generator(height: '250', width: '250')
     end
 end
