464fa4d7fe9fabd88949aa40ec73180b89d47417
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-30 20:56:28 -0600

    commit 17 to pool[peaceful-flying-cows-5bxqqv] at 20:56 - 30/01/2018

--- normangiantribe/config/routes.rb
+++ normangiantribe/config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get 'join', to: 'users#new'
   post 'users', to: 'users#create'
   get 'login', to: 'sessions#new'
-  
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 end
