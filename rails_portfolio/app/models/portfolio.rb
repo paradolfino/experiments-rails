53c8a2c4e3e9b7a3fa9dc044b70b26fb15ef6b20
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-30 19:33:10 -0600

    commit 20 to pool[brilliant-shining-cows-86z0rr] at 19:33 - 30/01/2018

--- rails_portfolio/app/models/portfolio.rb
+++ rails_portfolio/app/models/portfolio.rb
@@ -15,7 +15,8 @@ class Portfolio < ApplicationRecord
     after_initialize :set_defaults
 
     def :set_defaults
-
+        self.main_image ||= "http://via.placeholder.com/600x400"
+        self.thumb_image ||= "http://via.placeholder.com/350x200"
     end
 
 end
