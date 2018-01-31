bf7c84117640f90e90e7b2df6fe57ff00cc327fb
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-30 19:47:55 -0600

    commit 25 to pool[peaceful-living-horses-a03v0e] at 19:47 - 30/01/2018

--- rails_portfolio/app/models/skill.rb
+++ rails_portfolio/app/models/skill.rb
@@ -1,4 +1,10 @@
 class Skill < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }
     validates :percent_utilized, presence: true
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.badge ||= "http://placehold.it/250x250"
+    end
 end
