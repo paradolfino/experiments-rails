8e4a50ba350065474193355146d0d7fdba537d3b
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-30 19:14:22 -0600

    commit 8 to pool[lovely-flying-cabins-1761u6] at 19:14 - 30/01/2018

--- rails_portfolio/app/models/portfolio.rb
+++ rails_portfolio/app/models/portfolio.rb
@@ -5,4 +5,8 @@ class Portfolio < ApplicationRecord
     enum status: { draft: 0, published: 1 }
     extend FriendlyId
     friendly_id :title, use: :slugged
+
+    def self.subwoofer
+        where(subtitle: "Subwoofer")
+    end
 end
