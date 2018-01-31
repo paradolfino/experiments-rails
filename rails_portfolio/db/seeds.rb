2fa9aaf64bc6602a2f32d8ca876e3ca96abaa8ed
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-30 19:03:54 -0600

    commit 2 to pool[lovely-flying-cabins-1761u6] at 19:03 - 30/01/2018

--- rails_portfolio/db/seeds.rb
+++ rails_portfolio/db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 arr = ("a".."z").to_a
-rand(15).times {|topic| tar = []; rand(6).times {tar << arr[rand(arr.length)]}; Topic.create!(title: "#{tar.join("")} #{topic}")}
+rand(15).times {|topic| tar = []; rand(6).times {tar << arr[rand(arr.length)]}; Topic.create!(title: "#{tar.join("")} #{topic} Topic")}
 rand(25).times { |blog| Blog.create!(title: "My Blog #{blog}!", body: "#Test Content", topic_id: Topic.last.id) }
 rand(5).times { |skill| Skill.create!(title: "My Skill #{skill}!", percent_utilized: rand(100)) }
 
