fe1e2642529c32d93f9ddedf28ef8b04db7fd256
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-30 19:19:55 -0600

    commit 16 to pool[lovely-flying-cabins-1761u6] at 19:19 - 30/01/2018

--- rails_portfolio/config/routes.rb
+++ rails_portfolio/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
+  get 'subwoofer', to: "portfolios#subwoofer"
   
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   resources :blogs do
