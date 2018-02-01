928108c8a75b3fc49b32100f85ef4c6f959a2e39
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-31 19:17:01 -0600

    commit 5 to pool[lovely-jumping-rivers-bm3h00] at 19:17 - 31/01/2018

--- rails_portfolio/db/schema.rb
+++ rails_portfolio/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201010432) do
+ActiveRecord::Schema.define(version: 20180131014540) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -55,12 +55,6 @@ ActiveRecord::Schema.define(version: 20180201010432) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "badge"
-  end
-
-  create_table "technologies", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "topics", force: :cascade do |t|
