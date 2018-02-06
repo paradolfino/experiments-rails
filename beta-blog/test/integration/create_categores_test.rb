cc594666ff59abc372559559724003ea7f553598
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-05 19:36:05 -0600

    commit 8 to pool[lovely-running-cabins-6h4im4] at 19:36 - 05/02/2018

--- beta-blog/test/integration/create_categores_test.rb
+++ beta-blog/test/integration/create_categores_test.rb
@@ -7,7 +7,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
 
     test "get new category form and create category" do
-        sign_in_as(@user)
+        sign_in_as(@user, "password")
         get new_category_path
         assert_template 'categories/new'
         assert_difference 'Category.count', 1 do
