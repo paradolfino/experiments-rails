7f5d1f962152e1e654f0e6f4c01bf8564cd2801c
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-10 18:26:38 -0600

    commit 12 to pool[pretty-running-cabins-t04a7o] at 18:26 - 10/02/2018

--- rails_portfolio/db/migrate/20180211002003_devise_create_users.rb
+++ rails_portfolio/db/migrate/20180211002003_devise_create_users.rb
@@ -7,6 +7,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## Custom fields
+      t.string :first_name
+      t.string :last_name
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
