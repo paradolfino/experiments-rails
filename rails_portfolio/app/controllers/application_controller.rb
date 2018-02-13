591afed158bd693781efb062e9ebc8cfdddea8d6
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-12 19:29:34 -0600

    commit 20 to pool[peaceful-baking-horses-lo64tf] at 19:29 - 12/02/2018

--- rails_portfolio/app/controllers/application_controller.rb
+++ rails_portfolio/app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_params, if: :devise_controller?
 
   def configure_permitted_params
-    
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
