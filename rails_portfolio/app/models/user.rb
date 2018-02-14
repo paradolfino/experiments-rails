04a420bfb04d620b8274b7fa6863ce8db69d0ed9
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-13 19:16:01 -0600

    commit 8 to pool[happy-rolling-hills-rb076c] at 19:16 - 13/02/2018

--- rails_portfolio/app/models/user.rb
+++ rails_portfolio/app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         def first_name
+
+         end
+
+         def last_name
+
+         end
 end
