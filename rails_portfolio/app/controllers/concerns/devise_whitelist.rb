e27b7a9a9383913d073edeb4c810f9dcdf91c77d
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-13 19:05:16 -0600

    commit 5 to pool[lovely-flying-cows-20f6k3] at 19:05 - 13/02/2018

--- rails_portfolio/app/controllers/concerns/devise_whitelist.rb
+++ rails_portfolio/app/controllers/concerns/devise_whitelist.rb
@@ -1,3 +1,12 @@
 module DeviseWhitelist
     extend ActoveSupport::Concern
+
+    included do
+        before_filter :configure_permitted_params, if: :devise_controller?
+    end
+
+    def configure_permitted_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    end
 end
