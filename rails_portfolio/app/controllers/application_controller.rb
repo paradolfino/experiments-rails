a6e6f210191ea7c329f78c94918ecc7b2259d43b
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-15 20:25:52 -0600

    commit 3 to pool[pretty-shining-mountains-ycs03a] at 20:25 - 15/02/2018

--- rails_portfolio/app/controllers/application_controller.rb
+++ rails_portfolio/app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   before_action :set_source
 
   def set_source
-    session[:source] = params[:q]
+    session[:source] = params[:q] if params[:q]
   end
 end
