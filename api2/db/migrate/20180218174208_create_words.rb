87c0b0f4d8e8d19cadb5719c1d094dd3aaf286e7
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-18 11:47:42 -0600

    commit 9 to pool[rapid-shining-cabins-62t11z] at 11:47 - 18/02/2018

--- api2/db/migrate/20180218174208_create_words.rb
+++ api2/db/migrate/20180218174208_create_words.rb
@@ -3,7 +3,11 @@ class CreateWords < ActiveRecord::Migration[5.1]
     create_table :words do |t|
       t.string :eng_si
       t.string :eng_pl
-      t.string :nor
+      t.string :nor_si
+      t.string :nor_pl
+      t.string :nor_def
+      t.string :nor_defpl
+      
       t.timestamps
     end
   end
