e8db621b9a0b4bc3446955ababa50d942e60084d
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-18 14:00:52 -0600

    commit 4 to pool[serene-shining-rabbits-7183dx] at 14:00 - 18/02/2018

--- rails_portfolio/app/controllers/concerns/set_source.rb
+++ rails_portfolio/app/controllers/concerns/set_source.rb
@@ -1,6 +1,7 @@
 module SetSource
+    extend ActiveSupport::Concern
     included do
-
+        before_action :set_source
     end
     def set_source
         session[:source] = params[:q] if params[:q]
