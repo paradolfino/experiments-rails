d3b84b3d1ea18ff58112ed4b416832dacc58a459
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-18 15:01:27 -0600

    commit 18 to pool[happy-jumping-villages-3ntizm] at 15:01 - 18/02/2018

--- rails_portfolio/app/controllers/concerns/current_user_concern.rb
+++ rails_portfolio/app/controllers/concerns/current_user_concern.rb
@@ -1,3 +1,7 @@
 module CurrentUserConcern
+    extend ActiveSupport::Concern
 
+    def current_user
+        super || OpenStruct.new(first_name: "Guest", last_name: "Guest", email: "guest@guest.com")
+    end
 end
