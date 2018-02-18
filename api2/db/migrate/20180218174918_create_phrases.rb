49a0c25327da2472cf8977d732a4c9af0ed85493
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-18 11:51:15 -0600

    commit 14 to pool[rapid-shining-cabins-62t11z] at 11:51 - 18/02/2018

--- api2/db/migrate/20180218174918_create_phrases.rb
+++ api2/db/migrate/20180218174918_create_phrases.rb
@@ -3,6 +3,7 @@ class CreatePhrases < ActiveRecord::Migration[5.1]
     create_table :phrases do |t|
       t.text :english
       t.text :normansk
+      t.references :word, foreign_key: true
       t.timestamps
     end
   end
