4cb413ced275a6777ee1cf7c284d1dcb0406afa1
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-18 11:56:34 -0600

    commit 15 to pool[rapid-shining-cabins-62t11z] at 11:56 - 18/02/2018

--- api2/db/schema.rb
+++ api2/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218174208) do
+ActiveRecord::Schema.define(version: 20180218174918) do
+
+  create_table "phrases", force: :cascade do |t|
+    t.text "english"
+    t.text "normansk"
+    t.integer "word_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["word_id"], name: "index_phrases_on_word_id"
+  end
 
   create_table "words", force: :cascade do |t|
     t.string "eng_si"
