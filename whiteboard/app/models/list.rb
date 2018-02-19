15b4e5115886f5e757af4ba440a213dac5eec155
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-18 18:52:46 -0600

    commit 18 to pool[lovely-jumping-hills-5c7l8l] at 18:52 - 18/02/2018

--- whiteboard/app/models/list.rb
+++ whiteboard/app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
     acts_as_list
     has_many :cards, ->{ order(position: :asc)}, dependent: :destroy
-    validates :name, presence: trie
+    validates :name, presence: true
 end
