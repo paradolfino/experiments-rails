0c946746cd244a94df51095262ea60d47cf121e0
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-21 20:47:12 -0600

    commit 27 to pool[brilliant-jumping-rabbits-42222d] at 20:47 - 21/02/2018

--- rails_portfolio/app/helpers/application_helper.rb
+++ rails_portfolio/app/helpers/application_helper.rb
@@ -7,8 +7,7 @@ module ApplicationHelper
         if !current_user.is_a?(OpenStruct) 
             link_to "Logout", destroy_user_session_path, method: :delete 
         else 
-            "#{(link_to "Login", new_user_session_path)} <br/>
-            #{(link_to "Sign Up", new_user_registration_path)}".html_safe
+            "#{(link_to "Login", new_user_session_path)} <br/> #{(link_to "Sign Up", new_user_registration_path)}".html_safe
         end 
     end
 end
