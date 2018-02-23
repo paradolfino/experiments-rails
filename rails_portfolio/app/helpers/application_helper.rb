3917b691f73e7698e113a924035d9baed250257f
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-02-22 20:08:07 -0600

    commit 4 to pool[brilliant-running-mountains-cmvg77] at 20:08 - 22/02/2018

--- rails_portfolio/app/helpers/application_helper.rb
+++ rails_portfolio/app/helpers/application_helper.rb
@@ -1,7 +1,4 @@
 module ApplicationHelper
-    def sample_helper
-        "<p>My Helper</p>".html_safe
-    end
 
     def login_helper
         if !current_user.is_a?(OpenStruct) 
@@ -9,5 +6,9 @@ module ApplicationHelper
         else 
             "#{(link_to "Login", new_user_session_path)} <br/> #{(link_to "Sign Up", new_user_registration_path)}".html_safe
         end 
+    end
+
+    def sample_helper
+
     end
 end
