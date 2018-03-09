4db05ff63547b59ecf58aec98a400d53bf4611d5
viktharien <viktharien@zoho.com>
2018-03-08 20:25:56 -0600

    work with custom rubygem

--- rails_portfolio/app/controllers/application_controller.rb
+++ rails_portfolio/app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ end
 module VolanderViewTool
   class Renderer
     def self.copyright name, msg
-      "&copy; #{Time.now.year}"
+      "&copy; #{Time.now.year} | <strong>#{name}</strong> #{msg}".html_safe
     end
   end
 end
